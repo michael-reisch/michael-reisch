ac69d90d9cd1939451ceffd2f49515d58a8563c1
Michael Reisch <mreisch88@gmail.com>
2022-12-18 13:00:22 +1300

    building about, removing unnecessary files

---
+++ pages/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container } from '@chakra-ui/react'
+
+const About = (props) => {
+  return (
+    <Container>
+      <h1>Hello!</h1>
+      <p>
+        My name is Michael Reisch. I am a recent graduate of Dev Academy
+        Aotearoa looking to land my first job in the tech industry. I 
+      </p>
+    </Container>
+  )
+}
+
+export default About
